703f0f6531cfedf84af0995ca48ec7d8c1587ffe
Paweł Talarek <pawel.talarek@wp.pl>
2020-05-11 20:54:11 +0200

    add initial python code

---
+++ DM_RacePlanner.py
@@ -0,0 +1,85 @@
+import sys
+import ac
+import acsys 
+import csv
+import os
+import datetime
+import time
+import platform
+
+if platform.architecture()[0] == "64bit":
+  sysdir = "stdlib64"
+else:
+  sysdir = "stdlib"
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "dmrp", sysdir))
+os.environ['PATH'] = os.environ['PATH'] + ";."
+
+from dmrp import dmrp
+
+# set up mySQL coonnection
+con = dmrp.pymysql.connect('22707.v.tld.pl', 'admin22707_raceplanner', 
+    '9AeG6fZ6l9', 'baza22707_raceplanner')
+cur = con.cursor()
+
+# compound
+compound = 'NA'
+
+# lap valid
+lap_valid = 1
+  
+# is car in the pit lane
+current_inpit = 0
+
+# was car in the pit lane on the current lap
+current_lap_inpit = 0
+
+# current lap number
+lapnumber = 0
+
+# current date and file name
+now = datetime.date.today()
+driver_name = ac.getDriverName(0)
+track_name = ac.getTrackName(0)
+car = ac.getCarName(0)
+
+def acMain(ac_version):
+	
+	return "DM Race Planner"
+	
+def acUpdate(deltaT):
+	global current_inpit, lapnumber, current_lap_inpit, con, cur, compound, lap_valid
+	
+	# invalidate lap when more than 2 tyres off the track
+	if dmrp.info.physics.numberOfTyresOut > 2:
+		lap_valid_tmp = 0
+		if lap_valid_tmp != lap_valid:
+			lap_valid = lap_valid_tmp
+	
+	pit = ac.isCarInPitline(0)
+	if pit != current_inpit:
+	
+		# get compound after car left the pitlane
+		if pit == 0:
+			compound = dmrp.info.graphics.tyreCompound
+			
+		current_inpit = pit        	        	
+		if current_inpit == 1:
+			current_lap_inpit = current_inpit
+
+	lap = ac.getCarState(0, acsys.CS.LapCount)
+	
+	# new lap
+	if lap != lapnumber:
+		dt = time.strftime('%Y%m%d%H%M%S', time.localtime())
+		lapnumber = lap
+		last_lap = ac.getCarState(0, acsys.CS.LastLap)
+		
+		# upload lap to the databse
+		sql = "INSERT INTO `SessionData` (`lap`, `track`, `driver`, `car`, `time`, `pit`, `laptime`, `compound`, `valid`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+		cur.execute(sql, (lapnumber, track_name, driver_name, car, dt, current_lap_inpit, last_lap, compound, lap_valid))
+		con.commit()
+		
+		current_lap_inpit = 0
+		current_inpit = 0
+		lap_valid = 1
---
+++ dmrp/dmrp.py
@@ -0,0 +1,6 @@
+import ac
+import traceback
+import os
+import ctypes.wintypes
+from dmrp_libs import pymysql
+from dmrp_libs.sim_info import info
